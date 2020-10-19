3892f89fc57cb9e0e17cad261ed1cf9efb9e3c21
Michael Evan Kelly <email@michaelevan.com>
2020-10-19 08:05:43 -0700

    Consolidated the neutral colors

--- src/pages/color.js
+++ src/pages/color.js
@@ -1,22 +1,23 @@
 import React from "react"
+import _ from "lodash";
 
 import {
-    // Alert,
-    // Audio,
-    // BorderRadius,
-    // ColorFamilies,
-    // ColorRow,
-    // ColorSwatch,
-    // Download,
-    // FontFamily,
-    // FontSize,
-    // FontWeight,
-    // Heading,
-    Palette,
-    // Shadow,
-    // Space,
-    // Table,
-    // Video,
+  // Alert,
+  // Audio,
+  // BorderRadius,
+  // ColorFamilies,
+  // ColorRow,
+  ColorSwatch,
+  // Download,
+  // FontFamily,
+  // FontSize,
+  // FontWeight,
+  // Heading,
+  Palette,
+  // Shadow,
+  // Space,
+  // Table,
+  // Video,
 } from "@lekoarts/gatsby-theme-specimens"
 import {Typography} from "antd";
 import {AntPage} from "./../../src/components/"
@@ -27,50 +28,53 @@ import './../../src/css/custom.scss'
 const { Paragraph, Title } = Typography;
 
 export default function Home() {
-    //const { sortedColors } = useSortedColors("blue","color");
+  //const { sortedColors } = useSortedColors("blue","color");
+  const neutrals = _.concat(
+    useSortedColors("black","brand"),
+    useSortedColors("grey","brand"),
+    useSortedColors("white","brand")
+)
 
-    return (
-      <AntPage title="Color" icon="color">
-          <Paragraph>The CSAA color palette is aligned with color use in web applications and support for accessibility. The colors below constitute the expanded color palette for CSAA. Text colors displayed in light or dark tints represent CSAA's recommended accessible pairing with the color.</Paragraph>
-          <Title level={2}>Brand Colors</Title>
-          <Palette
-            colors={useSortedColors("blue","brand")}
-            mode="swatch"
-          />
-          <Palette
-            colors={useSortedColors("black","brand")}
-            mode="swatch"
-          />
-          <Palette
-            colors={useSortedColors("grey","brand")}
-            mode="swatch"
-          />
-          <Palette
-            colors={useSortedColors("white","brand")}
-            mode="swatch"
-          />
-          <Palette
-            colors={useSortedColors("red","brand")}
-            mode="swatch"
-          />
-          <Title level={2}>Spectrum</Title>
-          <Title level={3}>Blues</Title>
-          <Palette
-            colors={useSortedColors("blue","color")}
-          />
-          <Title level={3}>Greys</Title>
-          <Palette
-            colors={useSortedColors("grey","color")}
-          />
-          <Title level={3}>Reds</Title>
-          <Palette
-            colors={useSortedColors("red","color")}
-          />
-      </AntPage>
-    )
+  return (
+    <AntPage title="Color" icon="color">
+      <Paragraph>The CSAA color palette is aligned with color use in web applications and support for accessibility. The colors below constitute the expanded color palette for CSAA. Text colors displayed in light or dark tints represent CSAA's recommended accessible pairing with the color.</Paragraph>
+      <Title level={2}>Brand Colors</Title>
+      <Palette
+        colors={useSortedColors("blue","brand")}
+        mode="swatch"
+      />
+
+      <Palette
+        colors={neutrals}
+        mode="swatch"
+      />
+      <Palette
+        colors={useSortedColors("red","brand")}
+        mode="swatch"
+      />
+      <Title level={2}>Spectrum</Title>
+      <Title level={3}>Blues</Title>
+      <Palette
+        colors={useSortedColors("blue","color")}
+      />
+      <Title level={3}>Greys</Title>
+      <Palette
+        colors={useSortedColors("grey","color")}
+      />
+      <Title level={3}>Reds</Title>
+      <Palette
+        colors={useSortedColors("red","color")}
+      />
+    </AntPage>
+  )
 }
 
 /*
+
+          <ColorSwatch
+            color={black.color}
+            name={black.name}
+          />
       <Palette
         colors={useSortedColors("blue","color")}
         mode="swatch"
