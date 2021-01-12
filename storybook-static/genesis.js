38e2a8ba68ac0ab36d6722cf62047d5737eb0829
Michael Evan Kelly <email@michaelevan.com>
2021-01-12 10:20:17 -0800

    Adjusted static paths

---
+++ storybook-static/genesis.js
@@ -0,0 +1,174 @@
+export const typography = {
+  typefaces: [
+      {
+        name: "Raleway 400 (regular)",
+        family:{
+          name: "Raleway"
+        }
+      },
+      {
+        name: "Raleway 700 (bold)",
+        family:{
+          name: "Raleway"
+        }
+      },
+      {
+        name: "Cabin 400 (regular)",
+        family:{
+          name: "Cabin"
+        }
+      },
+  ],
+  text_styles: [
+      {
+        name: "raleway-xxxl",
+        family: {
+          token: "raleway_700_bold",
+          name: "Raleway"
+        },
+        fontSize : "69px",
+        lineHeight: "80px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "raleway-xxl",
+        family: {
+          token: "raleway_400_regular",
+          name: "Raleway"
+        },
+        fontSize : "55px",
+        lineHeight: "82px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "raleway-xl",
+        family: {
+          token: "raleway_700_bold",
+          name: "Raleway"
+        },
+        fontSize : "44px",
+        lineHeight: "66px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "raleway-l",
+        family: {
+          token: "raleway_400_regular",
+          name: "Raleway"
+        },
+        fontSize : "35px",
+        lineHeight: "62px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "raleway-m",
+        family: {
+          token: "raleway_700_bold",
+          name: "Raleway"
+        },
+        fontSize : "28px",
+        lineHeight: "48px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "raleway-s",
+        family: {
+          token: "raleway_700_bold",
+          name: "Raleway"
+        },
+        fontSize : "23px",
+        lineHeight: "40px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "raleway-xs",
+        family: {
+          token: "raleway_400_regular",
+          name: "Raleway"
+        },
+        fontSize : "18px",
+        lineHeight: "32px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "raleway-xs-bold",
+        family: {
+          token: "raleway_700_bold",
+          name: "Raleway"
+        },
+        fontSize : "18px",
+        lineHeight: "32px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "raleway-xxs",
+        family: {
+          token: "raleway_400_regular",
+          name: "Raleway"
+        },
+        fontSize : "16px",
+        lineHeight: "16px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "raleway-xxs-bold",
+        family: {
+          token: "raleway_700_bold",
+          name: "Raleway"
+        },
+        fontSize : "16px",
+        lineHeight: "16px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "raleway-xxxs",
+        family: {
+          token: "raleway_700_bold",
+          name: "Raleway"
+        },
+        fontSize : "14px",
+        lineHeight: "16px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "cabin-m",
+        family: {
+          token: "cabin_400_regular",
+          name: "Cabin"
+        },
+        fontSize : "18px",
+        lineHeight: "32px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "cabin-s",
+        family: {
+          token: "cabin_400_regular",
+          name: "Cabin"
+        },
+        fontSize : "14px",
+        lineHeight: "24px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "cabin-xs",
+        family: {
+          token: "cabin_400_regular",
+          name: "Cabin"
+        },
+        fontSize : "12px",
+        lineHeight: "16px",
+        letterSpacing : "0em"
+      },
+      {
+        name: "Body",
+        family: {
+          token: "cabin_400_regular",
+          name: "Cabin"
+        },
+        fontSize : "18px",
+        lineHeight: "32px",
+        letterSpacing : "0em"
+      },
+  ]
+};
--- storybook-static/static/fonts/RT-Raleway/extra/webfonts/README.md
+++
@@ -1,3 +0,0 @@
-# Webfonts
-
-These fonts were generated using [FontSquirrel](https://www.fontsquirrel.com/tools/webfont-generator "FontSquirrel Webfont Generator"). 
